e42e70fc110f28f098de8af94a8af70d230ffd9c
asgerami <amiraman467@gmail.com>
2025-02-09 15:07:13 +0300

    the great reset

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,16 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+const purgecss = require('@fullhuman/postcss-purgecss');
+const cssnano = require('cssnano');
+
+module.exports = {
+  plugins: [
+    require('tailwindcss')('./tailwind.js'),
+    require('autoprefixer'),
+    cssnano({
+      preset: 'default'
+    }),
+    purgecss({
+      content: ['./*.html', './src/**/*.jsx', './src/**/*.js'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
+  ]
+};
--- client/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const HOST = import.meta.env.VITE_SERVER_URL;
-
-export const AUTH_ROUTES = "api/auth";
-export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
